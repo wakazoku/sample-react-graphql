a0cb872691847244266e3b9b305c13b323a81b4b
wakazoku <wakazoku@gmail.com>
2020-12-27 21:50:09 +0900

    GraphQLでリポジトリ検索クエリを実装する

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
+import React, { Component } from "react";
 import client from "./client";
 import { ApolloProvider } from "react-apollo";
 import { Query } from "react-apollo";
-import { ME } from "./graphql.js";
+import { SEARCH_REPOSITORIES } from "./graphql.js";
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <h1>Hello GraphQL</h1>
-      <Query query={ME}>
-        {({ loading, error, data }) => {
-          if (loading) return "loading...";
-          if (error) return `Error! ${error.message}`;
-          return <p>{data.user.name}</p>;
-        }}
-      </Query>
-    </ApolloProvider>
-  );
+const VARIABLES = {
+  first: 5,
+  after: null,
+  before: null,
+  last: null,
+  query: "フロントエンドエンジニア",
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = VARIABLES;
+  }
+
+  render() {
+    const { query, first, last, before, after } = this.state;
+    return (
+      <ApolloProvider client={client}>
+        <h1>Hello GraphQL</h1>
+        <Query
+          query={SEARCH_REPOSITORIES}
+          variables={{ query, first, last, before, after }}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return "loading...";
+            if (error) return `Error! ${error.message}`;
+            console.log({ data });
+            return <div></div>;
+          }}
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
